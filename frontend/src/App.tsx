59f1fc2ea79bc24249e30f678b2ed2ef549aae69
Antony Chan <antony.chan.hn@gmail.com>
2022-09-05 12:04:00 +0800

    add emotion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -34,13 +34,21 @@ import '@ionic/react/css/display.css'
 /* Theme variables */
 import './theme/variables.css'
 
+// import page
+import WorkerLoginPage from './pages/WorkerLoginPage'
+
 setupIonicReact()
 
 const App: React.FC = () => (
 	<IonApp>
 		<IonReactRouter>
 			<IonRouterOutlet>
-				<Route exact path='/' component={HomePage} />
+				{/* <Route exact path='/' component={HomePage} /> */}
+				<Route
+					exact
+					path='/workerLoginPage'
+					component={WorkerLoginPage}
+				/>
 			</IonRouterOutlet>
 		</IonReactRouter>
 	</IonApp>
