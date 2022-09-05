c1d66926b9ed873b09005a796ed6ae6e8998b744
Yin <chenglokyin@gmail.com>
2022-09-05 15:17:06 +0800

    main css,UserTabBar upload

---
+++ frontend/src/components/UserTabBar.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { BsFillMicFill } from 'react-icons/bs';
+export default  function DetailsOfRecordingContent() {
+  
+
+    return (   
+    <div>
+        <BsFillMicFill />
+    </div>
+   
+      )
+}
