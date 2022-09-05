66c427c4590585411619f4c6d062241934fb4228
Yin <chenglokyin@gmail.com>
2022-09-05 11:47:16 +0800

    DetailsOfRecordingContent component up load

---
+++ frontend/src/components/DetailsOfRecordingContent.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+// const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+export default  function DetailsOfRecordingContent() {
+  
+
+    return (   
+    <div>
+        <div>請以下順序清楚說明:</div>
+            <ol>
+                <li>你所屬的十八區名稱</li>
+                <li>你需要維修的類別</li>
+                <li>具體維修內容</li>
+            </ol>
+    </div>
+   
+      )
+}
