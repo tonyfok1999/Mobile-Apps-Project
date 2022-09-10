f06834b7ba29c71576bbe51134350486449a3cb0
Antony Chan <antony.chan.hn@gmail.com>
2022-09-10 17:14:49 +0800

    add worker order page

--- frontend/src/pages/WorkerOrderPage.tsx
+++ frontend/src/pages/WorkerOrderPage.tsx
@@ -2,6 +2,7 @@
 import { css } from '@emotion/react'
 import { IonContent, IonPage } from '@ionic/react'
 import React, { useEffect, useState } from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function WorkerOrderPage() {
 	const [ordersInfo, setOrdersInfo] = useState<
@@ -23,14 +24,37 @@ export default function WorkerOrderPage() {
 		}[]
 	>()
 
+	const [referenceTable, setReferenceTable] = useState<
+		[
+			{ id: number; region_id: number; district: string }[], //districts
+			{ id: number; type: string }[], // service types
+			{
+				id: number
+				type: string
+				service_type_id: number
+				subtype: string
+			}[] // service subtypes
+		]
+	>()
+
 	useEffect(() => {
-		const fetchData = async () => {
+		const fetchOrdersData = async () => {
 			const res = await fetch('http://localhost:8000/orders')
-			const fetchData = await res.json()
-			setOrdersInfo(fetchData)
+			const data = await res.json()
+			setOrdersInfo(data)
 		}
-		fetchData()
+
+		const fetchReferenceTable = async () => {
+			const res = await fetch('http://localhost:8000/referencesTable')
+			const data = await res.json()
+			setReferenceTable(data)
+		}
+
+		fetchReferenceTable()
+		fetchOrdersData()
 	}, [])
+
+	// referenceTable[1].filter((type) => type.id == referenceTable[2].filter((subType) => subType.id == {orderInfo.service_subtype_id})[0].service_type_id)[0].type
 
 	return (
 		<IonPage
@@ -55,7 +79,42 @@ export default function WorkerOrderPage() {
 				{ordersInfo &&
 					ordersInfo.map((orderInfo) => (
 						<div className='order' key={orderInfo.id}>
-							{orderInfo.id}
+							<NavLink to='/orderDetailPage'>
+								<div>{orderInfo.working_address}</div>
+								<div>{orderInfo.id}</div>
+								<div>
+									服務範圍
+									<span>
+										{
+											referenceTable![1].filter(
+												(type) =>
+													type.id ==
+													referenceTable![2].filter(
+														(subType) =>
+															subType.id ==
+															orderInfo.service_subtype_id
+													)[0].service_type_id
+											)[0].type
+										}
+									</span>
+								</div>
+								<div>
+									維修類別
+									<span>
+										{
+											referenceTable![2].filter(
+												(subType) =>
+													subType.id ==
+													orderInfo.service_subtype_id
+											)[0].subtype
+										}
+									</span>
+								</div>
+								<div>
+									預算<span>{orderInfo.budget}</span>
+								</div>
+								<NavLink to='/tabs/chatlist'>聯絡客戶</NavLink>
+							</NavLink>
 						</div>
 					))}
 			</IonContent>
