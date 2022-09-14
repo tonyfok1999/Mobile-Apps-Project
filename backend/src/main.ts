2c81358f0e84c458ffd5e5706c0f50728901e4f8
Antony Chan <antony.chan.hn@gmail.com>
2022-09-14 11:55:19 +0800

    remove jose and permit  to back-wnd for auth

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,12 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
-import express from "express";
-import cors from "cors";
-import { ConfigModule } from "@nestjs/config";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import express from 'express';
+import cors from 'cors';
+import { ConfigModule } from '@nestjs/config';
 
 ConfigModule.forRoot({
-  envFilePath: [".env"],
+  envFilePath: ['.env'],
 });
 
 async function bootstrap() {
@@ -14,39 +14,18 @@ async function bootstrap() {
 
   app.enableCors();
 
-  const config = new DocumentBuilder()
-    .setTitle("repair-matching-platform")
-    .setDescription("The platform API description")
-    .setVersion("1.0")
-    .addTag("platform")
-    .build();
+  const config = new DocumentBuilder().setTitle('repair-matching-platform').setDescription('The platform API description').setVersion('1.0').addTag('platform').build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+  SwaggerModule.setup('api', app, document);
 
   app.enableCors({
     origin: [process.env.REACT_URL],
   });
 
-  // app.use((req, res, next) => {
-  // 	cors({
-  // 		origin: [process.env.URL]
-  // 	})
-  // 	next()
-  // })
-
   app.use((req, res, next) => {
     console.log(req.url);
     next();
   });
-
-  // app.use((req, res, next) => {
-  // 	express.urlencoded()
-  // 	next()
-  // })
-  // app.use((req, res, next) => {
-  // 	express.json()
-  // 	next()
-  // })
 
   await app.listen(8000, function () {
     console.log(`Server is listening on post:8000`);
