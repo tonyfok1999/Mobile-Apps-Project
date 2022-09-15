3435d0ff489257ca8576a152b2ff2b3cb77c6704
Yin <chenglokyin@gmail.com>
2022-09-15 13:31:05 +0800

    auth fix

--- backend/src/worker-auth/stratgies/local.strategy.ts
+++ backend/src/worker-auth/stratgies/local.strategy.ts
@@ -1,12 +1,15 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-local';
+import { IStrategyOptions, Strategy } from 'passport-local';
 import { WorkerAuthService } from '../worker-auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly workerAuthService: WorkerAuthService) {
-    super();
+    super({
+      usernameField: 'email',
+      passwordField: 'password',
+    } as IStrategyOptions);
   }
 
   async validate(email: string, password: string) {
