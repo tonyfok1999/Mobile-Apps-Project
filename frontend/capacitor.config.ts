bef6690f93175239a612abc1e9d3e5343d0bd0c9
Yin <chenglokyin@gmail.com>
2022-09-19 13:55:22 +0800

    google auth fix bug

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -4,7 +4,18 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'frontend',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins:{
+    GoogleAuth: {
+      scopes: [
+        'profile',
+        'email'
+      ],
+      serverClientId: '708322933526-0359al7b0ul1qll3i971rqu49jb7d7co.apps.googleusercontent.com', // On my project I'm using the real server client id
+      forceCodeForRefreshToken: true
+    }
+
+  }
 };
 
 export default config;
