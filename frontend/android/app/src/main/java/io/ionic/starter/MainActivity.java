212042e0f3b84c7b8d8b537b83ab871f82159cba
Antony Chan <antony.chan.hn@gmail.com>
2022-09-20 18:00:23 +0800

    fix google login

--- frontend/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ frontend/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,14 @@
 package io.ionic.starter;
 
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        registerPlugin(GoogleAuth.class);
+    }
+}
